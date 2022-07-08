510f2920a505dc4571a42deb1589bdfc42aba02e
Matthijs van Otterdijk <matthijs@terminusdb.com>
2022-07-08 16:31:51 +0200

    Autooptimize for the server case, where optimizer runs in background

--- auto-optimize.pl
+++ auto-optimize.pl
@@ -1,10 +1,16 @@
 :- module('plugins/auto_optimize', []).
 :- use_module(core(api)).
+:- use_module(library(http/http_server)).
 
 optimize_chance(system_descriptor{}, C) =>
     C = 0.1.
-optimize_chance(_D, C) =>
+optimize_chance(D, C), database_descriptor{} :< D =>
     C = 0.1.
+optimize_chance(D, C), repository_descriptor{} :< D =>
+    C = 0.1.
+optimize_chance(D, C), branch_descriptor{} :< D =>
+    C = 0.1.
+optimize_chance(_, _) => false.
 
 should_optimize(Descriptor) :-
     optimize_chance(Descriptor, Chance),
@@ -24,8 +30,21 @@ all_descriptor(Descriptor, Parent_Descriptor) :-
     get_dict(database_descriptor, Descriptor, Intermediate_Descriptor),
     all_descriptor(Intermediate_Descriptor, Parent_Descriptor).
 
-plugins:post_commit_hook(Validation_Objects, _Meta_Data) :-
+optimize_all(Validation_Objects) :-
     forall((member(V, Validation_Objects),
             all_descriptor(V.descriptor, Descriptor),
             should_optimize(Descriptor)),
            optimize(Descriptor)).
+
+:- multifile thread_pool:create_pool/1.
+thread_pool:create_pool(terminusdb_optimizer) :-
+    current_prolog_flag(cpu_count, Count),
+    thread_pool_create(terminusdb_optimizer, Count, []).
+
+plugins:post_commit_hook(Validation_Objects, _Meta_Data) :-
+    (   http_server_property(_, _)
+    ->  thread_create_in_pool(terminusdb_optimizer,
+                              optimize_all(Validation_Objects),
+                              _,
+                              [wait(false)])
+    ;   optimize_all(Validation_Objects)).
